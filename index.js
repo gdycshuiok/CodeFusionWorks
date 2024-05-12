3ff858d29917ccf0dcc027ed7c884b1a23febbca
gdycshuiok <ssjcanticanteli@hotmail.com>
2024-05-12 16:06:53 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-function levelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(currentLevel);
-  }
-  return result;
-}
+const word = "hello";
+const reversedWord = word.split("").reverse().join("");
+console.log(reversedWord); // olleh
