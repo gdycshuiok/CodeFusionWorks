e1c5356c8fc939c0ee6ca897f93112e432918ba1
gdycshuiok <ssjcanticanteli@hotmail.com>
2024-05-13 17:59:11 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+function deserialize(data) {
+  if (!data.length) return null;
+  const root = new TreeNode(data.shift());
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    const leftVal = data.shift();
+    if (leftVal !== undefined) {
+      node.left = new TreeNode(leftVal);
+      queue.push(node.left);
+    }
+    const rightVal = data.shift();
+    if (rightVal !== undefined) {
+      node.right = new TreeNode(rightVal);
+      queue.push(node.right);
+    }
+  }
+  return root;
+}
